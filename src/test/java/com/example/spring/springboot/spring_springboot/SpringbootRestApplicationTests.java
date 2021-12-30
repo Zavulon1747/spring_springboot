17debd3dfb3625437a37f242ed892fe566b2a3ef
Zavulon1747 <d1mas322@yandex.ru>
2021-12-30 11:40:04 +0400

    SpringBoot Rest

---
+++ src/test/java/com/example/spring/springboot/spring_springboot/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.springboot.spring_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
